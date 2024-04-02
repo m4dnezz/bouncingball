76e30dd1a2110ca142316386df82315113c4105e
Niclas <niclas.persson01@gmail.com>
2024-04-02 19:10:30 +0200

    Updated code to flake8 standard

--- simulation_main.py
+++ simulation_main.py
@@ -1,15 +1,11 @@
-from pygame.gfxdraw import circle
-from drawing import aacirlce
 import pygame
 import os
-import time as t
-from math import acos, atan2, sin, cos, sqrt, pi
-from colors import *
+from colors import bg, all_colors, main_colors, whitest
 from objects import Balls
-from constants import *
+from constants import fps, width, height, bigr, frames
 import random
-import traceback
 import numpy as np
+from drawing import aacirlce
 
 # Centers window
 x, y = 1360 - width, 40
@@ -29,102 +25,92 @@ pygame.mixer.init()
 # font = pygame.font.Font('freesansbold.ttf', 15)
 
 number_of_balls = 1
-for i in range(1,number_of_balls+1):
-    random_color = main_colors[random.randint(0,len(main_colors)-1)]
-    random_radius = random.randint(5,20)
-    random_x_offset = random.randint(10,50)
-    random_y_offset = random.randint(10,50)
-    Balls(str(random_color), random_color,random_radius,0,400+random_x_offset, 150+random_y_offset, "golf_ball.wav")
-    Balls.balls[-1].vely = random.randint(1,5) * random.choice((-1, 1))
-    Balls.balls[-1].velx = random.randint(1,2) * random.choice((-1, 1))
+for i in range(1, number_of_balls + 1):
+    random_color = main_colors[random.randint(0, len(main_colors) - 1)]
+    random_radius = random.randint(5, 20)
+    random_x_offset = random.randint(10, 50)
+    random_y_offset = random.randint(10, 50)
+    Balls(str(random_color), random_color, random_radius, 0, 400 + random_x_offset, 150 + random_y_offset, "golf_ball.wav")
+    Balls.balls[-1].vely = random.randint(1, 5) * random.choice((-1, 1))
+    Balls.balls[-1].velx = random.randint(1, 2) * random.choice((-1, 1))
 
 # redball   = Balls("red ball", red, 8, 0, width//2-bigr+20, height//2-59, "bm.wav")
-#redball   = Balls("red ball", golden, 8, 0, width//2-bigr+10, height//2, "golf_ball.wav")
-#redball.vely = -5
-#greenball = Balls("green ball", algeablue, 8, 0, width//2+bigr-20, height//2-50, "golf_ball.wav")
+# redball   = Balls("red ball", golden, 8, 0, width//2-bigr+10, height//2, "golf_ball.wav")
+# redball.vely = -5
+# greenball = Balls("green ball", algeablue, 8, 0, width//2+bigr-20, height//2-50, "golf_ball.wav")
 # yellowball = Balls("green ball", magenta2, 8, 0, width//3, height//2,"trm.wav")
 # blueball = Balls("green ball", blue, 8, 0, width*2//3+5, height//2, "trm.wav")
 # greenball = Balls("green ball", green, 12, 0, width//2+70, height//2-60)
 
 pause = False
-start_sim = False 
+start_sim = False
 
 
 while start_sim is False:
-    
     screen.fill(bg)
-    screen.blit(background, [0,0])
+    screen.blit(background, [0, 0])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_TAB:
                 start_sim = True
-    
-
     pygame.display.update()
     clock.tick(fps)
 
 
 while True:
-    try:
-        screen.fill(bg)
-        aacirlce(bigr, width//2, height//2, whitest, 1)
+    screen.fill(bg)
+    aacirlce(bigr, width // 2, height // 2, whitest, 1)
 
-        for ball in Balls.balls:
-            if len(ball.track)> 2 and Balls.trail:
-                pygame.draw.aalines(screen, ball.trail_color, False, ball.track, 2)
-        for ball in Balls.balls:
-            ball.drawball(screen)
-            if not pause:
-                ball.collision_handling()
-                ball.motion()
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    for ball in Balls.balls:
+        if len(ball.track) > 2 and Balls.trail:
+            pygame.draw.aalines(screen, ball.trail_color, False, ball.track, 2)
+    for ball in Balls.balls:
+        ball.drawball(screen)
+        if not pause:
+            ball.collision_handling()
+            ball.motion()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pause = not pause
+            if event.key == pygame.K_t:
+                Balls.trail = not Balls.trail
+                # if trail is False:
+                #     for ball in Balls.balls:
+                #         ball.track.clear()
+            if event.key == pygame.K_c:
+                Balls.balls[0].trail_color = all_colors[random.randint(0, len(all_colors) - 1)]
+            if event.key == pygame.K_PLUS:
+                Balls.balls[0].radius *= 1.1
+            if event.key == pygame.K_MINUS:
+                Balls.balls[0].radius /= 1.1
+            if event.key == pygame.K_UP:
+                Balls.balls[0].vely *= 1.1
+            if event.key == pygame.K_DOWN:
+                Balls.balls[0].vely /= 1.1
+            if event.key == pygame.K_b:
+                Balls.add_ball()
+            if event.key == pygame.K_ESCAPE:
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    pause = not pause
-                if event.key == pygame.K_t:
-                    Balls.trail = not Balls.trail
-                    # if trail is False:
-                    #     for ball in Balls.balls:
-                    #         ball.track.clear()
-                if event.key == pygame.K_c:
-                    Balls.balls[0].trail_color = all_colors[random.randint(0,len(all_colors)-1)]
-                if event.key == pygame.K_PLUS:
-                    Balls.balls[0].radius *= 1.1
-                if event.key == pygame.K_MINUS:
-                    Balls.balls[0].radius /= 1.1
-                if event.key == pygame.K_UP:
-                    Balls.balls[0].vely *= 1.1
-                if event.key == pygame.K_DOWN:
-                    Balls.balls[0].vely /= 1.1
-                if event.key == pygame.K_b:
-                    Balls.add_ball()
-                if event.key == pygame.K_ESCAPE:
-                    quit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pos())
-                if np.linalg.norm(np.subtract(pygame.mouse.get_pos(),(width//2,height//2))) + Balls.balls[0].radius + 5 < bigr:
-                    Balls.balls[0].posx, Balls.balls[0].posy = pygame.mouse.get_pos()
-                    Balls.balls[0].velx, Balls.balls[0].vely = 0.0001, 0.0001 # Avoid division with 0
-                    Balls.balls[0].track = list()
-                else:
-                    print("Cannot move ball outside limits")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
+            if np.linalg.norm(np.subtract(pygame.mouse.get_pos(), (width // 2, height // 2))) + Balls.balls[0].radius + 5 < bigr:
+                Balls.balls[0].posx, Balls.balls[0].posy = pygame.mouse.get_pos()
+                Balls.balls[0].velx, Balls.balls[0].vely = 0.0001, 0.0001  # Avoid division with 0
+                Balls.balls[0].track = list()
+            else:
+                print("Cannot move ball outside limits")
 
-        
+    pygame.display.update()
+    clock.tick(fps)
+    frames += 1
 
-        pygame.display.update()
-        clock.tick(fps)
-        frames += 1
-    except Exception as e:
-        with open("error_log.txt","w") as f:
-            traceback.print_exc(file=f)
-            pygame.quit()
 
-#TODO: Everything only works with one ball, no collisions between balls
-#TODO: Mute button
-#TODO: Remove balls from sim
+# TODO: Everything only works with one ball, no collisions between balls
+# TODO: Mute button
+# TODO: Remove balls from sim
